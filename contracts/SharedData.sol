32db14f282f97faa8778f70425cdea2635f883eb
metal-oopa <sudip5banerjee1974@gmail.com>
2023-05-31 01:20:06 +0530

    add chainlink keeper functions

--- contracts/SharedData.sol
+++ contracts/SharedData.sol
@@ -3,56 +3,56 @@ pragma solidity ^0.8.0;
 
 // building a library for shared data
 library SharedData {
-  struct HumanDetails {
-    string name;
-    string dateOfBirth;
-    string gender;
-    string homeAddress;
-    string phoneNumber;
-    string pronouns;
-    string email;
-    string occupation;
-    address payable policyHolderWalletAddress;
-  }
-  struct RevivalRule {
-    uint128 revivalPeriod;
-    uint256 revivalAmount;
-  }
+    struct HumanDetails {
+        string name;
+        string dateOfBirth;
+        string gender;
+        string homeAddress;
+        string phoneNumber;
+        string pronouns;
+        string email;
+        string occupation;
+        address payable policyHolderWalletAddress;
+    }
+    struct RevivalRule {
+        uint128 revivalPeriod;
+        uint256 revivalAmount;
+    }
 
-  struct Policy {
-    HumanDetails policyHolder;
-    uint128 polilcyTenure;
-    uint128 gracePeriod;
-    uint128 timeBeforeCommencement;
-    uint256 premiumToBePaid;
-    uint256 totalCoverageByPolicy;
-    bool hasClaimed;
-    bool isPolicyActive;
-    bool isClaimable;
-    bool isTerminated;
-    bool hasFundedForCurrentMonth;
-    RevivalRule revivalRule;
-    string policyDetails;
-    PolicyType policyType;
-    address payable policyManagerContractAddress;
-  }
+    struct Policy {
+        HumanDetails policyHolder;
+        uint128 polilcyTenure;
+        uint128 gracePeriod;
+        uint128 timeBeforeCommencement;
+        uint256 premiumToBePaid;
+        uint256 totalCoverageByPolicy;
+        bool hasClaimed;
+        bool isPolicyActive;
+        bool isClaimable;
+        bool isTerminated;
+        bool hasFundedForCurrentMonth;
+        RevivalRule revivalRule;
+        string policyDetails;
+        PolicyType policyType;
+        address payable policyManagerContractAddress;
+    }
 
-  enum PolicyType {
-    Life,
-    Health
-  }
+    enum PolicyType {
+        Life,
+        Health
+    }
 
-  struct Nominee {
-    HumanDetails nomineeDetails;
-    uint128 nomineeShare;
-  }
+    struct Nominee {
+        HumanDetails nomineeDetails;
+        uint128 nomineeShare;
+    }
 
-  struct LifePolicyParams {
-    Nominee[] nominees;
-    uint256 maturityCoverage;
-  }
+    struct LifePolicyParams {
+        Nominee[] nominees;
+        uint256 maturityCoverage;
+    }
 
-  struct HealthPolicyParams {
-    uint64 copaymentPercentage;
-  }
+    struct HealthPolicyParams {
+        uint64 copaymentPercentage;
+    }
 }
