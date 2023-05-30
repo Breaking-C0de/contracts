88c5557cb288c37107709822ca1334e500b970e2
Aaroh Srivastava <aarohsjcian1@gmail.com>
2023-05-30 23:39:06 +0530

    hardhat config and helper files initialised

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,34 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+const POLYGON_MUMBAI_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL ||
+  "https://polygon-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic";
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const POLYGONSCAN_API_KEY =
+  process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
+const REPORT_GAS = process.env.REPORT_GAS || false;
+
 module.exports = {
   solidity: "0.8.18",
   settings: {
@@ -7,5 +37,35 @@ module.exports = {
       runs: 100,
     },
     viaIR: true,
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+      chainId: 5,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+      chainId: 80001,
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+      default: 1,
+    },
   },
 };
