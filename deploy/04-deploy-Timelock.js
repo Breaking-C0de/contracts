9427080f1224004e54d54d95da19e193aa507340
debajyotidhar111 <debajyotidhar111@gmail.com>
2023-06-11 19:16:23 +0530

    Added governance contracts and deploy script

--- deploy/04-deploy-Timelock.js
+++ deploy/04-deploy-Timelock.js
@@ -1,16 +1,22 @@
 const { verify } = require("../utils/verify")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
-
+const { ethers } = require("hardhat")
 const MIN_DELAY = 86400 // 1 day
 module.exports = async function (hre) {
     const { getNamedAccounts, deployments, network } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const signers = await ethers.getSigners()
     log("----------------------------------------------------")
     log("Deploying TimeLock and waiting for confirmations...")
     const timeLock = await deploy("TimeLock", {
         from: deployer,
-        args: [MIN_DELAY, [], [], deployer],
+        args: [
+            MIN_DELAY,
+            [signers[1].address, signers[2].address, signers[3].address, signers[4].address],
+            [],
+            deployer,
+        ],
         log: true,
         waitConfirmations: networkConfig[network.config.chainId].blockConfirmations || 1,
     })
