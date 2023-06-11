d53a9e54e4c13b67d0c4b6f7dc91824ff4590e41
debajyotidhar111 <debajyotidhar111@gmail.com>
2023-06-11 19:23:23 +0530

    add move-block and move-time

---
+++ utils/move-block.js
@@ -0,0 +1,11 @@
+const { network } = require("hardhat")
+export async function moveBlocks(amount) {
+    console.log("Moving blocks...")
+    for (let index = 0; index < amount; index++) {
+        await network.provider.request({
+            method: "evm_mine",
+            params: [],
+        })
+    }
+    console.log(`Moved ${amount} blocks`)
+}
